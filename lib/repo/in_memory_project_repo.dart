82675d603dff7646e7a40663f168baeecb46f1de
Pimoltip <pimolthip.s@ku.th>
2025-10-03 15:26:51 +0700

    Delete lib directory
    ลบlibเก่า

--- lib/repo/in_memory_project_repo.dart
+++
@@ -1,9 +0,0 @@
-import '../models/project.dart';
-
-class InMemoryProjectRepo {
-  final _items = <Project>[];
-
-  List<Project> all() => List.unmodifiable(_items);
-  List<Project> byTag(String tag) => _items.where((e) => e.tag == tag).toList();
-  void add(Project p) => _items.add(p);
-}
